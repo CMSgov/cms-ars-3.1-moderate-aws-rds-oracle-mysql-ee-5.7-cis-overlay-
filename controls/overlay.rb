1222c2f4a6e4996e30a84664757fedae99017155
Haynes Jr <dhaynes@mitre.org>
2019-04-19 08:57:51 -0400

    adding overlay

---
+++ controls/overlay.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+include_controls 'cms-ars-3.1-moderate-oracle-mysql-ee-5.7-cis-overlay' do
+  # There are currently no changes required for the Oracle MySQL EE 5.7 CIS Benchmark
+  # tailored for CMS ARS 3.1 Moderate to support AWS RDS Oracle MySQL EE 5.7 for CMS ARS
+  # 3.1 Moderate.
+end
